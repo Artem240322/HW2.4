aaafe887c31f3743f0002282d9dc2c3a959136c8
ArtemCH240322 <sun2383@mail.ru>
2022-06-22 09:58:32 +0300

    Первый комит. Ловим ошибки. адание 1.

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,20 +1,9 @@
 package ru.skypro;
 
 public class Main {
-    public static void main (String[] args){
-        try {
-            check("test", "pass", "pass");
-        } check (WrongLoginException e){
-            throw new RuntimeException(e);
-        } check(WrongPasswordException e);
-            throw new RuntimeException(e);
-        }
-    }
+    public static void main(String[] args) {
+        check("test", "pass", "pass");
 
-    private static boolean check(
-            String login,
-            String password,
-            String confirmPassword) throws WrongLoginException, WrongPasswordException {
 
     }
 }
--- src/ru/skypro/Validator.java
+++ src/ru/skypro/Validator.java
@@ -1,4 +1,26 @@
 package ru.skypro;
 
 public class Validator {
+
+    private Validator() {
+    }
+
+    public static boolean check(String login,
+                                String password,
+                                String confirmPassword){
+        try {
+            validate(login, password, confirmPassword);
+            return true;
+        } catch (WrongLoginException | WrongPasswordException e){
+            System.out.println(e.getMessage());
+            return false;
+    }
 }
+
+    private static void validate(String login,
+                                 String password,
+                                 String confirmPassword) throws WrongLoginException, WrongPasswordException {
+
+    }
+}
+
