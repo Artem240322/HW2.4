95a103e14487912ad6c606ea24cc4cb97ff8b6b9
ArtemCH240322 <sun2383@mail.ru>
2022-06-22 11:46:14 +0300

    Первый комит. Ловим ошибки. адание 1. Залание 2. -равки.Залания 3-9. Правки.

--- src/ru/skypro/Validator.java
+++ src/ru/skypro/Validator.java
@@ -24,18 +24,24 @@ public class Validator {
     private static void validate(String login,
                                  String password,
                                  String confirmPassword) throws WrongLoginException, WrongPasswordException {
-        if (Objects.isNull(login) || login.length() > 20 || !containsValidSymbols(login)) {
+        if (Objects.isNull(login) || login.length() > 20 ) {
             throw new WrongLoginException("Длинна логина должна быть <= 20");
         }
+        if (!containsValidSymbols(login)){
+            throw new WrongLoginException("Логин содержит невалидные сымволы");
+        }
         if (isNotValidPassword(password) || isNotValidPassword(confirmPassword)) {
-            throw new WrongPasswordException("Длинна пароля должна быть <20");
+            throw new WrongPasswordException("Длинна пароля должна быть < 20");
+        }
+        if (!containsValidSymbols(password)) {
+            throw new WrongPasswordException("Пароль содержит невалидные символы");
         }
         if (!password.equals(confirmPassword)) {
             throw new WrongPasswordException("Пароли должны совпадать!");
         }
     }
     private static boolean isNotValidPassword(String password) {
-        return Objects.isNull(password) || password.length() >= 20 || !containsValidSymbols(password);
+        return Objects.isNull(password) || password.length() >= 20;
     }
 
     private static boolean containsValidSymbols(String s) {
